1c084e2ce548625beedab9cd6539f64312985ace
AvidDollars <dima.kol@seznam.cz>
2024-03-23 15:23:29 +0100

    this should trigger new deployment

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -13,7 +13,7 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Just Testing deploying Svelte on GitHub Pages :)</h1>
+  <h1>This should trigger new deployment :)</h1>
 
   <div class="card">
     <Counter />
